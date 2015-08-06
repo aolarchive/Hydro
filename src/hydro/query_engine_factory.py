180108d274bb8719e185b30cf023ebda3e17b7c0
Moshe Basanchig <moshe.basanchig@convertro.com>
2015-08-06 17:07:14 +0300

    Lazy initialize the connection handler in the query engine factory. This way, it won't attempt to initialize a logger (via the Base class) too early in the application lifecycle.

--- src/hydro/query_engine_factory.py
+++ src/hydro/query_engine_factory.py
@@ -6,10 +6,17 @@ from connector_factory import ConnectorHandler
 
 class QueryEngineFactory(object):
     # cache_engines and conn_handler are singletons, hence they are static
-    conn_handler = ConnectorHandler()
+    # moreover, conn_handler is lazy initialized
+    conn_handler = None
+
+    @classmethod
+    def __get_conn_handler(cls):
+        if cls.conn_handler is None:
+            conn_handler = ConnectorHandler()
+        return cls.conn_handler
 
     @classmethod
     def get_query_engine(cls, engine_type, cache_engine_instance, execution_plan, logger):
-        cls.conn_handler.set_logger(logger)
+        cls.__get_conn_handler().set_logger(logger)
         query_engine = QueryEngine(engine_type, cls.conn_handler, cache_engine_instance, execution_plan, logger)
         return query_engine
